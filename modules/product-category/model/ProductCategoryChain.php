7bb3b47d1003b85828a4f13cad50ff38eca889eb
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-06-17 11:03:40 +0700

    Add chain field on model

--- modules/product-category/model/ProductCategoryChain.php
+++ modules/product-category/model/ProductCategoryChain.php
@@ -12,7 +12,12 @@ class ProductCategoryChain extends \Mim\Model
 
     protected static $table = 'product_category_chain';
 
-    protected static $chains = [];
+    protected static $chains = [
+        'product' => [
+            'model' => 'Product\\Model\\Product',
+            'field' => 'id'
+        ]
+    ];
 
     protected static $q = [];
 }
